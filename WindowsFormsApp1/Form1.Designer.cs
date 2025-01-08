b4cfa764898f5392d797fe61e3bc9a319b725190
Nischal Khanal <nischal.khanal.2021@hotmail.com>
2025-01-08 15:54:16 +0545

    few changes

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -192,7 +192,9 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.OptionC);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Temperature Converter";
             this.ResumeLayout(false);
