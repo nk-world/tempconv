edeee587d180d63b9ef874428abb27686751440b
Nischal Khanal <nischal.khanal.2021@hotmail.com>
2025-01-09 12:50:03 +0545

    minor UI change

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -177,7 +177,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(333, 217);
+            this.button1.Location = new System.Drawing.Point(352, 218);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(91, 26);
             this.button1.TabIndex = 11;
